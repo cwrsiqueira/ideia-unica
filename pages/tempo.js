7ce41ed743a89f71cf8f3daa7ce27cf8988b8d4a
cwrsiqueira <cwrsiqueira@msn.com>
2022-03-28 20:09:51 -0300

    testando getStaticProps

---
+++ pages/tempo.js
@@ -0,0 +1,25 @@
+const Tempo = (props) => {
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
+
+    return (
+        <div>
+            <div>{dynamicDateString} (dinâmico) </div>
+            <div>{props.staticDateString} (estático) </div>
+        </div>
+    )
+}
+
+export const getStaticProps = async () => {
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return {
+        props:{
+            staticDateString
+        },
+        revalidate:1
+    }
+}
+
+export default Tempo;
