c2a52dceb664658dbc726581b8809add99722281
cwrsiqueira <cwrsiqueira@msn.com>
2022-03-28 19:55:04 -0300

    incluindo Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 function Home() {
-    return <div>Home</div>
+    return (
+        <div>
+            <div>Home</div>
+            <Link href="/sobre">
+                <a>Ir para Sobre</a>
+            </Link>
+        </div>
+    )
 }
 
 export default Home;
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function Sobre() {
+    return (
+        <div>
+            <div>Sobre</div>
+            <Link href="/">
+                <a>Ir para Home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Sobre;
