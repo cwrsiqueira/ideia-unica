0a1ff7d1f67cc685a13d58e0afb3997d7c14f606
cwrsiqueira <cwrsiqueira@msn.com>
2022-03-28 21:25:01 -0300

    tentando acessos o devsbarber via api em localhost

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,29 @@
 async function tempo(Request, Response) {
     const dynamicDate = new Date();
 
+    const requestData = {
+        "email": "teste@email.com",
+        "password": 123456
+    }
+
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(requestData),
+        headers: new Headers({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }),
+    };
+
+    const response = await fetch("http://127.0.0.1:8000/api/auth/login", requestOptions);
+    const user = await response.json();
+
+    const resBarbers = await fetch("http://127.0.0.1:8000/api/barbers?token="+user.token);
+    const barbers = await resBarbers.json();
 
     Response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        barbers
     })
 }
 
